5cbc7d1c629e2b0a7ebe531729014ef25acf0d4a
guy <88316240+GNagahashi@users.noreply.github.com>
2022-08-29 16:20:32 +0900

    edit drone_ctrl_gui.py/main()

--- scripts/drone_ctrl_gui.py
+++ scripts/drone_ctrl_gui.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+
+# >>> import sys; sys.version
+# '2.7.17 (default, Jul  1 2022, 15:56:32) \n[GCC 7.5.0]'
+
 import tkinter as tk
 from functools import partial
 import rospy
@@ -6,7 +10,146 @@ from std_msgs.msg import String
 
 
 def main():
-    pass
+    # Create and setting for app window
+    app = tk.Tk()
+    app.title('Drone App')  # window title
+    app.minsize(500, 280)  # window size
+    app.resizable(False, False)  # fixed window size(both width and height)
+
+    # Create Publisher node
+    # code
+
+    # Create frame
+    frame_label = tk.Frame(
+        app,
+        padx = 12,  # padding[px]
+        pady = 12,  # padding[px]
+        bg = '#ff0000'
+    )
+    frame_btn = tk.Frame(
+        app,
+        padx = 12,  # padding[px]
+        pady = 12,  # padding[px]
+        bg = '#00ff00'
+    )
+    subframe_btn = tk.Frame(
+        frame_btn,
+        bg = '#0000ff',
+    )
+
+    # Create widget
+    # label
+    label_h1 = tk.Label(
+        frame_label,
+        anchor = tk.W,  # align left
+        font = ('Helvetica', 12),
+        # bg = '#afeeee',
+        text = 'History:',
+    )
+    label_p1 = tk.Label(
+        frame_label,
+        anchor = tk.W,  # align left
+        font = ('Helvetica', 12),
+        # bg = '#fa5500',
+        text = '',
+    )
+    # button
+    button_start = tk.Button(
+        subframe_btn,
+        width = 10,  # use font size
+        height = 3,  # use font size
+        font = ('Helvetica', 12),
+        text = 'Start',
+    )
+    button_grab = tk.Button(
+        subframe_btn,
+        width = 10,  # use font size
+        height = 3,  # use font size
+        font = ('Helvetica', 12),
+        text = 'Grab',
+    )
+    button_forward = tk.Button(
+        subframe_btn,
+        width = 10,  # use font size
+        height = 3,  # use font size
+        font = ('Helvetica', 12),
+        text = 'Forward',
+    )
+    button_backward = tk.Button(
+        subframe_btn,
+        width = 10,  # use font size
+        height = 3,  # use font size
+        font = ('Helvetica', 12),
+        text = 'Backward',
+    )
+    button_left = tk.Button(
+        subframe_btn,
+        width = 10,  # use font size
+        height = 3,  # use font size
+        font = ('Helvetica', 12),
+        text = 'Left',
+    )
+    button_right = tk.Button(
+        subframe_btn,
+        width = 10,  # use font size
+        height = 3,  # use font size
+        font = ('Helvetica', 12),
+        text = 'Right',
+    )
+
+    # Set widget on top of a frame
+    label_h1.pack(
+        side = tk.LEFT,  # for this widget
+        padx = (0, 6),  # margin(left, right)   
+    )
+    label_p1.pack(
+        side = tk.LEFT,  # for this widget
+        padx = (0, 6),  # margin(left, right)
+    )
+    button_start.grid(
+        column = 4,
+        row = 2,
+    )
+    button_grab.grid(
+        column = 3,
+        row = 2,
+    )
+    button_forward.grid(
+        column = 1,
+        row = 0,
+    )
+    button_backward.grid(
+        column = 1,
+        row = 2,
+    )
+    button_left.grid(
+        column = 0,
+        row = 1,
+    )
+    button_right.grid(
+        column = 2,
+        row = 1,
+    )
+
+    # Set frame on top of a window
+    frame_label.pack(
+        expand = True,  # stretchable
+        fill = tk.BOTH,  # stretch direction
+        side = tk.TOP,  # for this frame
+        anchor = tk.N,  # for widgets in this frame
+    )
+    subframe_btn.pack(
+        side = tk.TOP,  # for this frame
+        anchor = tk.CENTER,  # for widgets in this frame
+    )
+    frame_btn.pack(
+        expand = True,  # stretchable
+        fill = tk.BOTH,  # stretch direction
+        side = tk.TOP,  # for this frame
+        anchor = tk.N,  # for widgets in this frame
+    )
+
+    app.mainloop()
 
 
 if __name__ == '__main__':
