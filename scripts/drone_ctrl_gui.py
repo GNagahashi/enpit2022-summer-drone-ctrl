8fda2b0bfba2f85eea38b04e54bcce80c1f8c8ce
guy <88316240+GNagahashi@users.noreply.github.com>
2022-08-29 15:21:33 +0900

    added file

---
+++ scripts/drone_ctrl_gui.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import tkinter as tk
+from functools import partial
+import rospy
+from std_msgs.msg import String
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
