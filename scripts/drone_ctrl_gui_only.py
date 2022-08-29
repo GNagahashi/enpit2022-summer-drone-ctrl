283bd5fc0d658fedcf61a139f444c26bffaec30d
guy <88316240+GNagahashi@users.noreply.github.com>
2022-08-30 00:13:33 +0900

    edit (added EventHandler class)

--- scripts/drone_ctrl_gui_only.py
+++ scripts/drone_ctrl_gui_only.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
+# >>> import sys; sys.version
+# '2.7.17 (default, Jul  1 2022, 15:56:32) \n[GCC 7.5.0]'
 
 """
 This program don't send message for ROS Topic.
 Only GUI.
 """
 
-# >>> import sys; sys.version
-# '2.7.17 (default, Jul  1 2022, 15:56:32) \n[GCC 7.5.0]'
 
 import tkinter as tk
 # from functools import partial
@@ -112,7 +112,7 @@ def main():
     # Set widget on top of a frame
     label_h1.pack(
         side = tk.LEFT,  # for this widget
-        padx = (0, 6),  # margin(left, right)   
+        padx = (0, 6),  # margin(left, right)
     )
     label_p1.pack(
         expand = True,  # stretchable
@@ -164,25 +164,34 @@ def main():
     )
 
     # Bind function
-    app.bind('<ButtonPress>', lambda e: ctrl_by_button(e, label_p1))
-    # app.bind('<ButtonPress>', lambda e: ctrl_by_button(e, label_p1))
-
-    # label_p1.update_idletasks()
-    # print('label_p1.winfo_width(): {}'.format(label_p1.winfo_width()))
+    e_handler = EventHandler(app)
+    app.bind('<ButtonPress>', lambda e: e_handler.drone_ctrl_by_button(e, label_p1))
 
     app.mainloop()
 
 
-def ctrl_by_button(e, label):
-    # print('ctrl_by_button')
-    # print('type(e.widget[\'text\'])', type(e.widget['text']))
-    # print('e.widget[\'text\']', e.widget['text'])
-    if e.widget.widgetName == 'button':
-        for i in TXT_LIST:
-            if e.widget['text'].lower() == i:
-                # label['text'] = e.widget['text'] + ', ' + label['text']
+class EventHandler(object):
+    def __init__(self, root):
+        self.__state = True
+        self.root_frame = root
+
+    def drone_ctrl_by_button(self, e, label):
+        if self.__state and e.widget.widgetName == 'button':  # key can press and widget is button?
+            key = ''.join([key for key in TXT_LIST if e.widget['text'].lower() == key])
+            if key:  # button is in the list?
                 label['text'] = e.widget['text']
-                print('Send message: {}'.format(i))  # similar ROS_INFO()
+                print('Send message: {}'.format(key))  # similar ROS_INFO()
+                self.disable_handler()
+        else:
+            print('Event handler is disable or Press invalid button')
+
+    def disable_handler(self):
+        self.root_frame.after(3000, self.enable_handler)
+        self.__state = False
+
+    def enable_handler(self):
+        self.__state = True
+        print('Event handler is enable')
 
 
 if __name__ == '__main__':
